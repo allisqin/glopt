6fa17b92ae7ca0cc5e286844a11cb266929c6ffa
allisqin <allisqin@gmail.com>
2024-10-09 22:55:05 +0300

    project

---
+++ js/icon_customImage.js
@@ -0,0 +1,54 @@
+//map 
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.748019, 37.627238],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #ec644b; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Glopt',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: '../icons/map/bullit.png',
+            // Размеры метки.
+            iconImageSize: [31, 31],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        }),
+
+        myPlacemarkWithContent = new ymaps.Placemark([55.748019, 37.627238], {
+            hintContent: 'Собственный значок метки с контентом',
+            iconContent: 'Glopt'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#imageWithContent',
+            // Своё изображение иконки метки.
+            iconImageHref: '',
+            // Размеры метки.
+            iconImageSize: [62, 63],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-24, -24],
+            // Смещение слоя с содержимым относительно слоя с картинкой.
+            iconContentOffset: [15, 15],
+            // Макет содержимого.
+            iconContentLayout: MyIconContentLayout
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark)
+        .add(myPlacemarkWithContent);
+});
